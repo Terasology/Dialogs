dfa2846b53d3fdc23770bb7b079c81b9275c47db
anuar2k <anuar2k@outlook.com>
2018-01-11 20:49:37 +0100

    Goes together with API fix - Dialog should build again

--- src/main/java/org/terasology/dialogs/DialogSystem.java
+++ src/main/java/org/terasology/dialogs/DialogSystem.java
@@ -26,6 +26,7 @@ import org.terasology.dialogs.components.DialogComponent;
 import org.terasology.dialogs.components.DialogPage;
 import org.terasology.dialogs.components.DialogResponse;
 import org.terasology.engine.SimpleUri;
+import org.terasology.engine.subsystem.config.BindsSubsystem;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.prefab.Prefab;
@@ -64,7 +65,7 @@ public class DialogSystem extends BaseComponentSystem {
     private NUIManager nuiManager;
 
     @In
-    private InputSystem inputSystem;
+    BindsSubsystem bindsSubsystem;
 
     @In
     private AssetManager assetManager;
@@ -184,7 +185,7 @@ public class DialogSystem extends BaseComponentSystem {
 
     private String createTalkText() {
         SimpleUri id = new SimpleUri("engine:frob");
-        List<Input> inputs = inputSystem.getInputsForBindButton(id);
+        List<Input> inputs = bindsSubsystem.getInputsForBindButton(id);
         String text = "Press ";
         for (Input input : inputs) {
             if (input.getType() == InputType.KEY) {
