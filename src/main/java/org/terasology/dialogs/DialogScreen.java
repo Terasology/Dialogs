a25a1baba0313c16b83a546956178f24a2175b13
DarkWeird <foxflameless@gmail.com>
2020-08-20 21:46:27 +0300

    fix: NUI extraction compatibility

--- src/main/java/org/terasology/dialogs/DialogScreen.java
+++ src/main/java/org/terasology/dialogs/DialogScreen.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.dialogs;
 
 import com.google.common.collect.Lists;
@@ -21,11 +8,11 @@ import org.terasology.dialogs.action.PlayerAction;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.rendering.assets.texture.TextureRegion;
 import org.terasology.rendering.nui.CoreScreenLayer;
-import org.terasology.rendering.nui.UIWidget;
-import org.terasology.rendering.nui.databinding.ReadOnlyBinding;
-import org.terasology.rendering.nui.layouts.ColumnLayout;
-import org.terasology.rendering.nui.widgets.UIButton;
-import org.terasology.rendering.nui.widgets.UIImage;
+import org.terasology.nui.UIWidget;
+import org.terasology.nui.databinding.ReadOnlyBinding;
+import org.terasology.nui.layouts.ColumnLayout;
+import org.terasology.nui.widgets.UIButton;
+import org.terasology.nui.widgets.UIImage;
 import org.terasology.rendering.nui.widgets.browser.data.html.HTMLDocument;
 import org.terasology.rendering.nui.widgets.browser.ui.BrowserWidget;
 
--- src/main/java/org/terasology/notify/ui/DialogNotificationOverlay.java
+++ src/main/java/org/terasology/notify/ui/DialogNotificationOverlay.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2018 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.notify.ui;
 
@@ -22,12 +9,8 @@ import java.util.List;
 import org.terasology.assets.ResourceUrn;
 import org.terasology.engine.Time;
 import org.terasology.registry.In;
-import org.terasology.rendering.nui.Canvas;
-import org.terasology.rendering.nui.Color;
 import org.terasology.rendering.nui.CoreScreenLayer;
-import org.terasology.rendering.nui.databinding.DefaultBinding;
-import org.terasology.rendering.nui.widgets.UILabel;
-import org.terasology.rendering.nui.widgets.UIList;
+import org.terasology.nui.widgets.UIList;
 
 public class DialogNotificationOverlay extends CoreScreenLayer {
 
