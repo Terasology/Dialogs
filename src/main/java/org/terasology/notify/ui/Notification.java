721f5434c06caae88ba6dd958552ff2270311b4b
Tobias Nett <skaldarnar@googlemail.com>
2021-12-08 20:09:36 +0100

    style: update copyright
    And some additional CheckStyle fixes.

--- src/main/java/org/terasology/notify/ui/Notification.java
+++ src/main/java/org/terasology/notify/ui/Notification.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.notify.ui;
 
@@ -21,8 +8,8 @@ package org.terasology.notify.ui;
  */
 final class Notification {
 
-    final static long FADE_IN_TIME = 300;
-    final static long FADE_OUT_TIME = 300;
+    static final long FADE_IN_TIME = 300;
+    static final long FADE_OUT_TIME = 300;
 
     private final String text;
     private long timeAdded;
@@ -33,9 +20,6 @@ final class Notification {
         this.timeAdded = timeAdded;
     }
 
-    /**
-     * @return
-     */
     public String getText() {
         return text;
     }
